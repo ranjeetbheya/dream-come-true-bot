eb2c20175b14d039fee90ee8c821382986341131
ranjeetbheya <ranjeetrawat303@gmail.com>
2025-07-03 18:59:44 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_first_name = update.effective_user.first_name
@@ -11,7 +10,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(welcome_text)
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    help_text = "Here are some things you can do:\n" \                "/start - Begin your journey\n" \                "/vedicmath - Learn fast math tricks\n" \                "/abacus - Improve your mental calculation\n" \                "/ask - Ask any career/study question"
+    help_text = (
+        "Here are some things you can do:\n"
+        "/start - Begin your journey\n"
+        "/vedicmath - Learn fast math tricks\n"
+        "/abacus - Improve your mental calculation\n"
+        "/ask - Ask any career/study question"
+    )
     await update.message.reply_text(help_text)
 
 async def vedicmath(update: Update, context: ContextTypes.DEFAULT_TYPE):
